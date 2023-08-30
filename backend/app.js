57f35dda0dd0bd5397cef2cda77e195e32769665
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-08-30 09:06:19 +0530

    init commit

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+//const loginRouter = require('./')
+//const usersRoter
+//const productsRouter
+//const middleware
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+//app.use(middleware)
---
+++ backend/models.js/user.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+const isEmail = require('validator/lib/isEmail')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minLength: 4,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  passwordHash: String,
+  email: {
+    type: String,
+    validate: [isEmail, 'invalid email'],
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    minLength: 10,
+  },
+  avatarId: Number,
+  address: String,
+  cart: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      quantity: Number,
+    },
+  ],
+  purchaseHistory: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      quantity: Number,
+      date: Date,
+    },
+  ],
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
