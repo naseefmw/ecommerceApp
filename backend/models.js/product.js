0777eeaee3169559b4a5e36acd81cac530a77127
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-08-30 09:15:53 +0530

    product model added

--- backend/models.js/product.js
+++ backend/models.js/product.js
@@ -1,1 +1,31 @@
- 
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  brand: String,
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: String,
+  image: {
+    type: String,
+    required: true,
+  },
+  addedDate: Date,
+  rating: Number,
+})
+
+productSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._Id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Product', productSchema)
