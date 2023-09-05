3a9c95d84287ce958da4166fb95b716eb3935ff6
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-09-05 09:07:48 +0530

    added media queries and changed login page styling

--- frontend/src/components/HomePage/FilterBar.jsx
+++ frontend/src/components/HomePage/FilterBar.jsx
@@ -48,57 +48,60 @@ const FilterBar = () => {
   return (
     <div className="filternavbar">
       <TextField
+        sx={{ maxWidth: 750 }}
+        fullWidth
         size="small"
         label="Search"
         value={searchFilter}
         onChange={handleSearch}
-        sx={{ width: 360 }}
       ></TextField>
-      <FormControl size="small" sx={{ width: width }}>
-        <InputLabel>Sort By</InputLabel>
-        <Select label="Sort by" value={sortFilter} onChange={handleSort}>
-          <MenuItem value="DATE">New</MenuItem>
-          <MenuItem value="PRICE_ASC">Price Low to High</MenuItem>
-          <MenuItem value="PRICE_DESC">Price High to Low</MenuItem>
-          <MenuItem value="RATING">Rating</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl size="small" sx={{ width: width }}>
-        <InputLabel>Category</InputLabel>
-        <Select
-          label="Category"
-          value={categoryFilter}
-          onChange={handleCategory}
-        >
-          <MenuItem value="ALL">All</MenuItem>
-          <MenuItem value="Accessory">Accessory</MenuItem>
-          <MenuItem value="Laptop">Laptop</MenuItem>
-          <MenuItem value="SmartPhone">SmartPhone</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl size="small" sx={{ width: width }}>
-        <InputLabel>Brand</InputLabel>
-        <Select label="Brand" value={brandFilter} onChange={handleBrand}>
-          <MenuItem value="ALL">All</MenuItem>
-          <MenuItem value="Apple">Apple</MenuItem>
-          <MenuItem value="Google">Google</MenuItem>
-          <MenuItem value="HP">HP</MenuItem>
-          <MenuItem value="Samsung">Samsung</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl size="small" sx={{ width: width }}>
-        <InputLabel>Price</InputLabel>
-        <Select label="Price" value={priceFilter} onChange={handlePrice}>
-          <MenuItem value="ALL">All</MenuItem>
-          <MenuItem value="U10K">Under 10K</MenuItem>
-          <MenuItem value="10K20K">10K - 20K</MenuItem>
-          <MenuItem value="20K50K">20K - 50K</MenuItem>
-          <MenuItem value="O50K">Over 50K</MenuItem>
-        </Select>
-      </FormControl>
-      <Button variant="contained" color="error" onClick={handleReset}>
-        Clear
-      </Button>
+      <div className="filters">
+        <FormControl size="small" sx={{ width: width }}>
+          <InputLabel>Sort By</InputLabel>
+          <Select label="Sort by" value={sortFilter} onChange={handleSort}>
+            <MenuItem value="DATE">New</MenuItem>
+            <MenuItem value="PRICE_ASC">Price Low to High</MenuItem>
+            <MenuItem value="PRICE_DESC">Price High to Low</MenuItem>
+            <MenuItem value="RATING">Rating</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl size="small" sx={{ width: width }}>
+          <InputLabel>Category</InputLabel>
+          <Select
+            label="Category"
+            value={categoryFilter}
+            onChange={handleCategory}
+          >
+            <MenuItem value="ALL">All</MenuItem>
+            <MenuItem value="Accessory">Accessory</MenuItem>
+            <MenuItem value="Laptop">Laptop</MenuItem>
+            <MenuItem value="SmartPhone">SmartPhone</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl size="small" sx={{ width: width }}>
+          <InputLabel>Brand</InputLabel>
+          <Select label="Brand" value={brandFilter} onChange={handleBrand}>
+            <MenuItem value="ALL">All</MenuItem>
+            <MenuItem value="Apple">Apple</MenuItem>
+            <MenuItem value="Google">Google</MenuItem>
+            <MenuItem value="HP">HP</MenuItem>
+            <MenuItem value="Samsung">Samsung</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl size="small" sx={{ width: width }}>
+          <InputLabel>Price</InputLabel>
+          <Select label="Price" value={priceFilter} onChange={handlePrice}>
+            <MenuItem value="ALL">All</MenuItem>
+            <MenuItem value="U10K">Under 10K</MenuItem>
+            <MenuItem value="10K20K">10K - 20K</MenuItem>
+            <MenuItem value="20K50K">20K - 50K</MenuItem>
+            <MenuItem value="O50K">Over 50K</MenuItem>
+          </Select>
+        </FormControl>
+        <Button variant="contained" color="error" onClick={handleReset}>
+          Clear
+        </Button>
+      </div>
     </div>
   )
 }
