4d38ba4bd7c6f24f63458c0b8541cb1a114440e1
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-09-06 18:47:45 +0530

    product list style changed

--- frontend/src/components/HomePage/Product.jsx
+++ frontend/src/components/HomePage/Product.jsx
@@ -3,9 +3,11 @@ const Product = ({ product }) => {
   return (
     <div className="product">
       <img src={product.image} alt="product_image" />
-      <span id="product-name">{product.name}</span>
-      <h4>{product.rating} &#10025;</h4>
-      <h2>&#8377; {product.price}</h2>
+      <div className="project-captions">
+        <span id="product-name">{product.name}</span>
+        <h4 id="product-rating">{product.rating} &#10025;</h4>
+        <h2 id="product-price">&#8377; {product.price}</h2>
+      </div>
     </div>
   )
 }
